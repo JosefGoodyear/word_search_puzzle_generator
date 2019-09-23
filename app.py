5cf673e08d45b5cbfe001b9a9004f21f330ca354
Josef Goodyear <josefgoodyear@gmail.com>
2019-09-23 15:25:07 -0700

    use sessions instead of global variables

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 from random import randrange, choice
 from copy import deepcopy
 
-app = Flask(__name__)
 
-size = 20
-word_list = []
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+
 
 
 def horizontal_forward(word):
@@ -14,8 +17,8 @@ def horizontal_forward(word):
     while attempt < 10:
         attempt += 1
         collision = False
-        y = randrange(0, size)
-        x = randrange(0, size - len(word) + 1)
+        y = randrange(0, session.get('size'))
+        x = randrange(0, session.get('size') - len(word) + 1)
         for i in range(len(word)):
             if grid[y][x + i] != '_' and grid[y][x + i] != word[i]:
                 collision = True
@@ -35,8 +38,8 @@ def horizontal_backward(word):
     while attempt < 10:
         attempt += 1
         collision = False
-        y = randrange(0, size)
-        x = randrange(len(word), size)
+        y = randrange(0, session.get('size'))
+        x = randrange(len(word), session.get('size'))
         for i in range(len(word)):
             if grid[y][x - i] != '_' and grid[y][x - i] != word[i]:
                 collision = True
@@ -56,8 +59,8 @@ def vertical_down(word):
     while attempt < 10:
         attempt += 1
         collision = False
-        y = randrange(0, size - len(word) + 1)
-        x = randrange(0, size)
+        y = randrange(0, session.get('size') - len(word) + 1)
+        x = randrange(0, session.get('size'))
         for i in range(len(word)):
             if grid[y + i][x] != '_' and grid[y + i][x] != word[i]:
                 collision = True
@@ -77,8 +80,8 @@ def vertical_up(word):
     while attempt < 10:
         attempt += 1
         collision = False
-        y = randrange(len(word), size)
-        x = randrange(0, size)
+        y = randrange(len(word), session.get('size'))
+        x = randrange(0, session.get('size'))
         for i in range(len(word)):
             if grid[y - i][x] != '_' and grid[y - i][x] != word[i]:
                 collision = True
@@ -98,8 +101,8 @@ def diagonal_up(word):
     while attempt < 10:
         attempt += 1
         collision = False
-        y = randrange(len(word), size)
-        x = randrange(0, size - len(word) + 1)
+        y = randrange(len(word), session.get('size'))
+        x = randrange(0, session.get('size') - len(word) + 1)
         for i in range(len(word)):
             if grid[y - i][x + i] != '_' and grid[y - i][x + i] != word[i]:
                 collision = True
@@ -119,8 +122,8 @@ def diagonal_down(word):
     while attempt < 10:
         attempt += 1
         collision = False
-        y = randrange(0, size - len(word) + 1)
-        x = randrange(0, size - len(word) + 1)
+        y = randrange(0, session.get('size') - len(word) + 1)
+        x = randrange(0, session.get('size') - len(word) + 1)
         for i in range(len(word)):
             if grid[y + i][x + i] != '_' and grid[y + i][x+ i] != word[i]:
                 collision = True
@@ -137,7 +140,7 @@ def diagonal_down(word):
 def randomizer():
     """Randomly choose a placement function, weighted by placement_functions. If placement fails, return False"""
     placement_functions = [vertical_down] * 4 + [vertical_up] * 1 + [diagonal_up] * 2 + [diagonal_down] * 2 + [horizontal_backward] * 1 + [horizontal_forward] * 4
-    for word in word_list:
+    for word in session.get('word_list'):
         status = choice(placement_functions)(word)
         if status is False:
             return False
@@ -147,16 +150,16 @@ def randomizer():
 def fill_in_the_blanks():
     """ Fill in random letters for empty squares in grid"""
     alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for i in range(size):
-        for j in range(size):
+    for i in range(session.get('size')):
+        for j in range(session.get('size')):
             if grid[i][j] == '_':
                 grid[i][j] = choice(alpha)
 
 
 def initialize_grid():
     """ Replace all letters in grid with '_' """
-    for i in range(size):
-        for j in range(size):
+    for i in range(session.get('size')):
+        for j in range(session.get('size')):
             grid[i][j] = '_'
 
 
@@ -166,9 +169,9 @@ def populate_puzzle(new_word_list):
         initialize_grid()
     else:
         global grid
-        grid = [['_' for i in range(size)] for j in range(size)]
-    word_list.clear()
-    word_list.extend(new_word_list)
+        grid = [['_' for i in range(session.get('size'))] for j in range(session.get('size'))]
+    session.get('word_list').clear()
+    session.get('word_list').extend(new_word_list)
     status = randomizer()
     if status is False:
         return False
@@ -182,11 +185,13 @@ def populate_puzzle(new_word_list):
 def input_words():
     """ Take words from user and return links to a
     word search puzzle and answer key, or an error message"""
+    session['size'] = 20
+    session['word_list'] = []
     if request.method == 'POST':
         words = request.form.get('words').upper()
         new_word_list = words.split()
         for word in new_word_list:
-            if len(word) > size:
+            if len(word) > session.get('size'):
                 return render_template('error.html')
         status = populate_puzzle(sorted(new_word_list, key=len, reverse=True))
         if status is False:
@@ -198,14 +203,14 @@ def input_words():
 @app.route('/puzzle', strict_slashes=False)
 def puzzle():
     """ Get the generated word search puzzle """
-    return render_template('puzzle.html', puzzle=grid, words=word_list)
+    return render_template('puzzle.html', puzzle=grid, words=session.get('word_list'))
 
 
 @app.route('/answer', strict_slashes=False)
 def answer():
     """ Get the generated answer key s"""
-    return render_template('puzzle.html', puzzle=answer_grid, words=word_list)
+    return render_template('puzzle.html', puzzle=answer_grid, words=session.get('word_list'))
 
 """ disable app.run for deployment with gunicorn """
-# if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port='8000', debug='true')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8000', debug='true')
