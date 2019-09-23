a4738144e4f7e3942c77244c2414d297c12c4fbe
Josef Goodyear <josefgoodyear@gmail.com>
2019-09-23 10:54:19 -0700

    add check for failure on word length and number of words

--- app.py
+++ app.py
@@ -8,99 +8,120 @@ size = 20
 word_list = []
 
 def horizontal_forward(word):
-    while True:
+    attempt = 0
+    while attempt < 10:
+        attempt += 1
         collision = False
         y = randrange(0, size)
         x = randrange(0, size - len(word) + 1)
         for i in range(len(word)):
             if grid[y][x + i] != '_' and grid[y][x + i] != word[i]:
                 collision = True
-                print("collision")
                 break
         if collision is False:
-            print("found a spot")
             break
+    if attempt == 10:
+        return False
     for count, letter in enumerate(word):
         grid[y][x + count] = letter
+    return True
 def horizontal_backward(word):
-    while True:
+    attempt = 0
+    while attempt < 10:
+        attempt += 1
         collision = False
         y = randrange(0, size)
         x = randrange(len(word), size)
         for i in range(len(word)):
             if grid[y][x - i] != '_' and grid[y][x - i] != word[i]:
                 collision = True
-                print("collision")
                 break
         if collision is False:
-            print("found a spot")
             break
+    if attempt == 10:
+        return False
     for count, letter in enumerate(word):
         grid[y][x - count] = letter
+    return True
 def vertical_down(word):
-    while True:
+    attempt = 0
+    while attempt < 10:
+        attempt += 1
         collision = False
         y = randrange(0, size - len(word) + 1)
         x = randrange(0, size)
         for i in range(len(word)):
             if grid[y + i][x] != '_' and grid[y + i][x] != word[i]:
                 collision = True
-                print("collision")
                 break
         if collision is False:
-            print("found a spot")
             break
+    if attempt == 10:
+        return False
     for count, letter in enumerate(word):
         grid[y + count][x] = letter
+    return True
 def vertical_up(word):
-    while True:
+    attempt = 0
+    while attempt < 10:
+        attempt += 1
         collision = False
         y = randrange(len(word), size)
         x = randrange(0, size)
         for i in range(len(word)):
             if grid[y - i][x] != '_' and grid[y - i][x] != word[i]:
                 collision = True
-                print("collision")
                 break
         if collision is False:
-            print("found a spot")
             break
+    if attempt == 10:
+        return False
     for count, letter in enumerate(word):
         grid[y - count][x] = letter
+    return True
 def diagonal_up(word):
-    while True:
+    attempt = 0
+    while attempt < 10:
+        attempt += 1
         collision = False
         y = randrange(len(word), size)
         x = randrange(0, size - len(word) + 1)
         for i in range(len(word)):
             if grid[y - i][x + i] != '_' and grid[y - i][x + i] != word[i]:
                 collision = True
-                print("collision")
                 break
         if collision is False:
-            print("found a spot")
             break
+    if attempt == 10:
+        return False
     for count, letter in enumerate(word):
         grid[y - count][x + count] = letter
+    return True
 def diagonal_down(word):
-    while True:
+    attempt = 0
+    while attempt < 10:
+        attempt += 1
         collision = False
         y = randrange(0, size - len(word) + 1)
         x = randrange(0, size - len(word) + 1)
         for i in range(len(word)):
             if grid[y + i][x + i] != '_' and grid[y + i][x+ i] != word[i]:
                 collision = True
-                print("collision")
                 break
         if collision is False:
-            print("found a spot")
             break
+    if attempt == 10:
+        return False
     for count, letter in enumerate(word):
         grid[y + count][x + count] = letter
+    return True
 def randomizer():
     placement_functions = [vertical_down] * 4 + [vertical_up] * 1 + [diagonal_up] * 2 + [diagonal_down] * 2 + [horizontal_backward] * 1 + [horizontal_forward] * 4
     for word in word_list:
-        choice(placement_functions)(word)
+        status = choice(placement_functions)(word)
+        if status is False:
+            return False
+    return True
 def fill_in_the_blanks():
     alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for i in range(size):
@@ -119,22 +140,31 @@ def populate_puzzle(new_word_list):
         grid = [ [ '_' for i in range(size)] for j in range(size)]
     word_list.clear()
     word_list.extend(new_word_list)
-    randomizer()
+    status = randomizer()
+    if status is False:
+        return False
     global answer_grid
     answer_grid = deepcopy(grid)
     fill_in_the_blanks()
+    return True
 
 @app.route('/', methods=['POST', 'GET'])
 def input_words():
     if request.method == 'POST':
-        words = request.form.get("words").upper()
-        populate_puzzle(sorted(words.split(), key=len, reverse=True))
-        return render_template("home2.html")
-    return render_template("home.html")
+        words = request.form.get('words').upper()
+        new_word_list = words.split()
+        for word in new_word_list:
+            if len(word) > size:
+                return render_template('error.html')
+        status = populate_puzzle(sorted(new_word_list, key=len, reverse=True))
+        if status is False:
+            return render_template('error_2.html')
+        return render_template('home2.html')
+    return render_template('home.html')
 
 @app.route('/puzzle', strict_slashes=False)
 def puzzle():
-    return render_template("puzzle.html", puzzle=grid, words=word_list)
+    return render_template('puzzle.html', puzzle=grid, words=word_list)
     
 @app.route('/answer', strict_slashes=False)
 def answer():
